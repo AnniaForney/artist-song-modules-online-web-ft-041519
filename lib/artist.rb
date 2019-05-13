4973b5ef299a54b09a181c9d4e81f52aaad30d18
Annia Forney <anniaforney@gmail.com>
2019-05-13 15:45:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 require 'pry'
 require_relative '../config/environment'
-class Artist < Song 
+class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Memorable::InstanceMethods
