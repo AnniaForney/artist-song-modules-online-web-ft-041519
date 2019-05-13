2e0273fa00b852087df494f5b84691c1df6eced7
Annia Forney <anniaforney@gmail.com>
2019-05-13 16:39:33 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -3,11 +3,13 @@ module Memorable
   def reset_all
    self.all.clear
   end
-end
 
-  module InstanceMethods
   def count
     self.all.count
   end
+
 end
-end 
+
+  module InstanceMethods
+end
+end
